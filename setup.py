a333d10f5d23ba5e1cc0e3946ba0911d7621cdd6
Sangwon91 <integratedmailsystem@gmail.com>
2020-01-06 11:21:58 +0900

    Fix typo.

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 # Get dependencies from requirements.txt file.
-with open("requirements.txt", "w") as f:
+with open("requirements.txt", "r") as f:
     install_requires = f.readlines()
 
 setup(
@@ -9,10 +9,10 @@ setup(
     version="0.0.1",
     description="Construction of nanoporous materials"
                 " from topology and building blocks.",
-    install_requires=install_requires
+    install_requires=install_requires,
     author="Sangwon Lee",
     author_email="integratedmailsystem@gmail.com",
-    packages=find_packages(include=["pormake", "rmsd"])
-    python_requires=">=3.5"
+    packages=find_packages(include=["pormake", "rmsd"]),
+    python_requires=">=3.5",
     zip_safe=False
 )
